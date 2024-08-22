27dc309e7bbb824dfaa237b0c10ef63acf799230
RizkySyahrani <syahranirizky99@gmail.com>
2024-08-22 13:18:21 +0700

    re commit

--- public/ajax.js
+++ public/ajax.js
@@ -1,34 +1,16 @@
 const { getAllBooks } = require("../models/bookModel");
 
-$(document).ready(function () {
-  // Fetch all books
-  $.ajax({
-    url: "http://localhost:3000/api/books",
-    method: "GET",
-    success: function (data) {
-      const bookList = $("#bookList");
-      bookList.empty();
-      data.forEach((book) => {
-        bookList.append(`<li>${book.title} by ${book.author}</li>`);
-      });
-    },
-    error: function (err) {
-      console.error("Error fetching books:", err);
+// AJAX code
 
-      // AJAX code
+const ajax = new XMLHttpRequest();
+ajax.onload = function () {
+  const data = JSON.parse(ajax.responseText);
+  getAllBooks(data);
+};
 
-      const ajax = new XMLHttpRequest();
-      ajax.onload = function () {
-        const data = JSON.parse(ajax.responseText);
-        getAllBooks(data);
-      };
+const url = getAllBooks();
+ajax.open("GET");
+(url = "http://localhost:3000/api/books"), ajax.send();
 
-      const url = getAllBooks();
-      ajax.open("GET");
-      (url = "http://localhost:3000/api/books"), ajax.send();
-
-      //const response =  JSON.parse(ajax.responseText);
-      //console.log(response);
-    },
-  });
-});
+//const response =  JSON.parse(ajax.responseText);
+//console.log(response);
